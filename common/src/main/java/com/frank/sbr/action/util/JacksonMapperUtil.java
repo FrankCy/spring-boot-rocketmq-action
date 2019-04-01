10e80241f13a6096819b29bde5b557758fa09495
cyoung <cy880708@163.com>
2019-04-01 15:40:54 +0800

    add mq code

---
+++ common/src/main/java/com/frank/sbr/action/util/JacksonMapperUtil.java
@@ -0,0 +1,142 @@
+package com.frank.sbr.action.util;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * @description：json工具类 功能详细描述：提供bean与json之间的转换和list与json之间的转换
+ * @version 1.0
+ * @author: Yang.Chang
+ * @email: cy880708@163.com
+ * @date: 2018/12/4 下午2:12
+ * @mofified By:
+ */
+public class JacksonMapperUtil {
+
+    /** 记录日志的变量 */
+    private static final Logger logger        = LoggerFactory.getLogger(JacksonMapperUtil.class);
+    /** 静态ObjectMapper */
+    private ObjectMapper mapper;
+
+    private static JacksonMapperUtil jacksonMapper = new JacksonMapperUtil();
+
+    private static final int         ARRAY_MAX     = 1024;
+
+    /**
+     * 私有构造函数
+     */
+    private JacksonMapperUtil() {
+        mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    /**
+     * 获得ObjectMapper实例
+     * @return ObjectMapper
+     */
+    public static ObjectMapper getInstance() {
+        return jacksonMapper.mapper;
+    }
+
+    /**
+     * JSON对象转换为JavaBean
+     * @param json JSON对象
+     * @param valueType Bean类
+     * @return 泛型对象
+     */
+    public static <T> T jsonToBean(String json,
+                                   Class<T> valueType) {
+        if (json == null || json.length() == 0) {
+            return null;
+        }
+        try {
+            return getInstance().readValue(json, valueType);
+        } catch (JsonParseException e) {
+            logger.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
+     * JavaBean转换为JSON字符串
+     * @param bean JavaBean对象
+     * @return json字符串
+     */
+    public static String beanToJson(Object bean) {
+        StringWriter sw = new StringWriter();
+        JsonGenerator gen = null;
+        try {
+            gen = new JsonFactory().createJsonGenerator(sw);
+            getInstance().writeValue(gen, bean);
+            gen.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return sw.toString();
+    }
+
+    /**
+     * 功能描述: <br>
+     * @param <T>
+     * @param json
+     * @param clazz
+     * @return
+     * @throws IOException
+     * @throws JsonMappingException
+     * @throws JsonParseException
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> List<T> jsonToList(String json,
+                                         Class<T> clazz) {
+        T[] t = (T[]) Array.newInstance(clazz, ARRAY_MAX);
+        try {
+            t = (T[]) getInstance().readValue(json, t.getClass());
+            return (List<T>) Arrays.asList(t);
+        } catch (JsonGenerationException e) {
+            logger.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+    /**
+     * 功能描述: <br>
+     * @param t
+     * @return
+     */
+    public static String listToJson(List<?> t) {
+        try {
+            return getInstance().writeValueAsString(t);
+        } catch (JsonGenerationException e) {
+            logger.error(e.getMessage(), e);
+        } catch (JsonMappingException e) {
+            logger.error(e.getMessage(), e);
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        }
+        return null;
+    }
+
+}
--- provider/src/main/java/com/frank/sbr/action/provider/impl/SendServiceImpl.java
+++ provider/src/main/java/com/frank/sbr/action/provider/impl/SendServiceImpl.java
@@ -1,8 +1,22 @@
 package com.frank.sbr.action.provider.impl;
 
+import com.frank.sbr.action.config.Constants;
+import com.frank.sbr.action.config.DefaultMQProducerSingleton;
 import com.frank.sbr.action.provider.service.SendService;
+import com.frank.sbr.action.util.JsonUtil;
 import com.frank.sbr.action.vo.CompanyVO;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.MQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
 
 /**
  * @version 1.0
@@ -17,31 +31,73 @@ import org.springframework.stereotype.Service;
 @Service
 public class SendServiceImpl implements SendService {
 
+    private static final Logger logger = LoggerFactory.getLogger(SendServiceImpl.class);
+
+    @Override
     public int sendSimpleMessage(CompanyVO companyVO) {
-        System.out.println(companyVO.toString());
+        // 实例化生产者，并设置组名
+        MQProducer producer = DefaultMQProducerSingleton.newInstance();
+
+        // 启动实例
+        try {
+            String voString = JsonUtil.beanToJson(companyVO);
+            logger.info("voString : " + voString);
+
+            producer.start();
+
+            Message msg = new Message(Constants.MQ_DEFAULT_TOPIC,
+                    Constants.MQ_DEFAULT_TOPIC_TAG,
+                    "KEY_SIMPLE",
+                    (voString).getBytes(RemotingHelper.DEFAULT_CHARSET));
+            SendResult sendResult = producer.send(msg);
+
+            // 输出响应结果
+            logger.info("sendResult toString : " + sendResult.toString());
+
+            // 发送后关闭生产者
+            producer.shutdown();
+
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (RemotingException e) {
+            e.printStackTrace();
+        } catch (MQBrokerException e) {
+            e.printStackTrace();
+        }
+
         return 0;
     }
 
+    @Override
     public int sendOrderMessage() {
         return 0;
     }
 
+    @Override
     public int sendBroadcastingMessage() {
         return 0;
     }
 
+    @Override
     public int sendScheduleMessage() {
         return 0;
     }
 
+    @Override
     public int sendBatchMessage() {
         return 0;
     }
 
+    @Override
     public int sendFilterMessage() {
         return 0;
     }
 
+    @Override
     public int sendTransactionMessage() {
         return 0;
     }
